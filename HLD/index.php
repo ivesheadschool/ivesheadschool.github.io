0d475dff027241dc99541c4951c785d7b8bd2fd6
Iveshead School <39085731+ivesheadschool@users.noreply.github.com>
2018-05-22 22:21:40 +0100

    Update and rename index.html to index.php

---
+++ HLD/index.php
@@ -0,0 +1,50 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>HLD | Ivesheadschool GitHub</title>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css\options.css">
+  </head>
+  <body>
+    <nav>
+      <h1 id="LOGO">IVESHEADSCHOOL<span id="LOGO_SUB">FORM</span></h1>
+    </nav>
+    <h1 id="subTitle">Pick your Activity Year 9/10</h1>
+    <p id="subDisp">Please select your favorite option from the list below</p>
+    <div class="options">
+      <div class="opt">
+        <p>Football</p>
+        <input type="radio" name="radio">
+      </div>
+      <div class="opt">
+        <p>Basketball</p>
+        <input type="radio" name="radio">
+      </div>
+      <div class="opt">
+        <p>Hotkey</p>
+        <input type="radio" name="radio">
+      </div>
+      <div class="opt">
+        <p>Computer Science</p>
+        <input type="radio" name="radio">
+      </div>
+      <div class="opt">
+        <p>Football</p>
+        <input type="radio" name="radio">
+      </div>
+      <div class="opt">
+        <p>Basketball</p>
+        <input type="radio" name="radio">
+      </div>
+      <div class="opt">
+        <p>Hotkey</p>
+        <input type="radio" name="radio">
+      </div>
+      <div class="opt">
+        <p>Computer Science</p>
+        <input type="radio" name="radio">
+      </div>
+    </div>
+  </body>
+</html>
